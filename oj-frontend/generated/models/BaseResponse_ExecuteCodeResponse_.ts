d695516f57d0d1fa576f5948aebed98119ffa300
cao <cao137831@163.com>
2024-08-03 19:01:13 +0800

    完成代码在线测试功能，前台代码提交的展示待优化

---
+++ oj-frontend/generated/models/BaseResponse_ExecuteCodeResponse_.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { ExecuteCodeResponse } from "./ExecuteCodeResponse";
+
+export type BaseResponse_ExecuteCodeResponse_ = {
+  code?: number;
+  data?: ExecuteCodeResponse;
+  message?: string;
+};
---
+++ oj-frontend/generated/models/ExecuteCodeResponse.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { JudgeInfo } from "./JudgeInfo";
+
+export type ExecuteCodeResponse = {
+  judgeInfo?: JudgeInfo;
+  judgeInfoStatus?: string;
+  outputList?: Array<string>;
+  questionSubmitStatus?: string;
+};
